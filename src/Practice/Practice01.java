481f0e4f3f2d50e92fd6969a4ed3fb1e41044351
NakatsukaAsuka <nakatsuka_asuka@crossware.co.jp>
2020-08-11 14:17:04 +0900

    練習

---
+++ src/Practice/Practice01.java
@@ -0,0 +1,49 @@
+package Practice;
+
+public class Practice01 {
+
+    //定数
+    private static final String _2KETA = "  ";
+    private static final String _3KETA = "   ";
+
+    public static void main(String[] args) {
+        nineNine();
+    }
+
+
+    public static void nineNine() {
+        for (int col = 0; col <= 9; col++) {
+            for (int row = 0; row <= 9; row++) {
+                if (col == 0) {
+                    if (row == 0) {
+                        System.out.print(_3KETA);
+                    } else {
+                        System.out.print(row + _2KETA);
+                    }
+                } else {
+                    if (row == 0) {
+                        System.out.print(col);
+                    } else {
+                        String result = String.valueOf(row * col);
+                        String show = format(result);
+                        System.out.print(show);
+                    }
+                }
+
+
+            }
+            System.out.println();
+
+        }
+    }
+
+
+    public static String format(String target) {
+        String changeFour = target.replace("4", "*");
+        return String.format("%3s", changeFour);
+    }
+
+}
+
+
+
